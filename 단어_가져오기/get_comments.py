5ca1d32b47700bfeb65ce6ebd6ecc905eaf3b83b
gimyuna <126344568+gimyuna03@users.noreply.github.com>
2024-08-07 17:42:50 +0900

    Update get_comments.py

--- 단어_가져오기/get_comments.py
+++ 단어_가져오기/get_comments.py
@@ -6,5 +6,5 @@ mongo_uri = "mongodb+srv://gimyuna:dtw01060D@dl-dragons.gqslqxe.mongodb.net/?ret
 V_counter = get_comments(mongo_uri)
 
 # V_counter의 예시 항목 몇 개를 출력
-for word, count in V_counter.items():  # 상위 10개의 항목만 출력
+for word, count in V_counter.items():
     print(f"{word}: {count}")
